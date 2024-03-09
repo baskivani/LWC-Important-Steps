5ee4176775126c015a9ab4aaa562e977be0e4f22
baskivani <98594932+baskivani@users.noreply.github.com>
2024-03-09 21:18:56 +0300

    Create 01DataBindingTekYonlu.js

---
+++ 01DataBindingTekYonlu.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class A02DataBinding extends LightningElement {     
+
+// tek yonlu data binding
+isim = 'MAHMUT' ; 
+
+kisi = {
+   ismi:'Hasan',
+   yas: '50',
+   oyunlar:['tavla','okey','satranc']
+}
