fa5ab420af42c7e2a683de94c35d5cb6c705da47
baskivani <98594932+baskivani@users.noreply.github.com>
2024-03-10 12:03:07 +0300

    Update 03DataManipulasyon.js

--- 03DataManipulasyon.js
+++ 03DataManipulasyon.js
@@ -10,18 +10,23 @@ handleilkIsim(event) {
 
     //get methodu (get ile set kardestir)
 
-    ilkIsimGet ='Benim adim burada yazilacak'  
+    ilkIsimGet   
 handleilkIsimGet(event) {
 
         this.ilkIsimGet =event.target.value
     }
 
+    soyAdGet 
+    handleSoyadGet(e) {
+    
+            this.soyAdGet =(e.target.value).toUpperCase()
+        }  
     container
   get  getIlkIsim(){
 
 this.container =this.ilkIsimGet
 this.container = this.container.toLowerCase();
-this.container = this.container.charAt(0).toUpperCase() + this.  container.slice(1)
+this.container = this.container.charAt(0).toUpperCase() + this.  container.slice(1)+ ' ' +this.soyAdGet
 
 return this.container
     }
