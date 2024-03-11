d696d3b074114d5c011363a16e58955053b019a2
baskivani <98594932+baskivani@users.noreply.github.com>
2024-03-11 23:11:55 +0300

    Create 06ConditionalRenderingYeniYontem.js

---
+++ 06ConditionalRenderingYeniYontem.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class A06ConditonalRenderingYeniYontem extends LightningElement {
+
+// conditional rendering Yeni
+    durum1 = true
+    durum2 = false
+    durum3 = false
+
+
+}
