ead795001b2236ad758d87a355c21678ce363b7f
baskivani <98594932+baskivani@users.noreply.github.com>
2024-03-13 23:08:55 +0300

    Create 08ListRenderingObjectORnegi.js

---
+++ 08ListRenderingObjectORnegi.js
@@ -0,0 +1,31 @@
+import { LightningElement } from 'lwc';
+
+export default class A08ListRenderingObjectOrnegi extends LightningElement {
+
+    cicekListesi = [
+        {
+        id:1,
+        name:'Gul',
+        familya:'DikenliGuller',
+        imgUrl:'https://www.nolinacicek.com/yuklemeler/cicek/img_9907.jpeg'
+        } ,
+        {
+        id:2,
+         name:'Lale',
+         familya:'Lalegiller',
+        imgUrl:'https://www.nolinacicek.com/yuklemeler/cicek/pembe-gul-yesil-vazo_1.jpg'
+        },
+        {
+        id:3,
+         name:'Zambak',
+         familya:'Zambakgiller',
+         imgUrl:'https://www.nolinacicek.com/yuklemeler/cicek/img_9906.jpeg'
+        },
+        {
+         id:4,
+         familya:'Soganligiller',
+         name:'Sumbul',
+         imgUrl:'https://www.nolinacicek.com/yuklemeler/cicek/buket-11.jpg'
+        }
+        ]
+}
